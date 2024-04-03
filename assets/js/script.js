c0a013c4e81d8baf360818b49ae82dface66affa
Alex Gartland <alexgartland22@gmail.com>
2024-04-03 18:55:19 +0000

    Add questions with correct answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,93 @@
 document.addEventListener("DOMContentLoaded", function() {
+	let userAnswer = "";
 	let buttons = document.getElementsByTagName("button");
 
 	for (let button of buttons) {
+		//Get answer from user via event listener on the buttons
 		button.addEventListener("click", function() {
+			
 			if (this.getAttribute("data-type") === "answer-one") {
-				alert("You clicked a button");
+				userAnswer = "a";
 			}
             else if (this.getAttribute("data-type") === "answer-two") {
-				alert("You clicked 2 button");
+				userAnswer = "b";
 			}
             else if (this.getAttribute("data-type") === "answer-three") {
-				alert("You clicked 3 button");
+				userAnswer = "c";
 			}
             else if (this.getAttribute("data-type") === "answer-four") {
-				alert("You clicked 4 button");
+				userAnswer = "d";
 			}
-            else {
-				let gameType = this.getAttribute("data-type");
-				runGame(gameType);
+
+			//Determine if user has the correct answer to question
+			if (document.getElementById("sports-question").textContent == "How many test match wickets does Shane Warne have?") {
+				if (userAnswer == "d") {
+					alert("That's correct!");
+					askQuestion()
+				} 
+				else {
+					alert("That's not correct =(");
+					askQuestion();
+				}
+			}
+			else if (document.getElementById("sports-question").textContent == "How many times have Livepool won the Champions League?") {
+				if (userAnswer == "b") {
+					alert("That's correct");
+					askQuestion();
+				}
+				else {
+					alert("That's not correct =(");
+					askQuestion();
+				}
+			}
+            else if (document.getElementById("sports-question").textContent == "Where is the Snooker UK Championship held?") {
+				if (userAnswer == "a") {
+					alert("That's correct");
+					askQuestion()
+				}
+				else {
+					alert("That's not correct =(");
+					askQuestion();
+				}
+			}
+			else if (document.getElementById("sports-question").textContent == "Which of these rugby teams are NOT part of the 6Nations?") {
+				if (userAnswer == "c") {
+					alert("That's correct");
+					askQuestion()
+				}
+				else {
+					alert("That's not correct =(");
+					askQuestion();
+				}
+			}
+			else if (document.getElementById("sports-question").textContent == "Who has won the men's singles at Wimbledon the most times?") {
+				if (userAnswer == "a") {
+					alert("That's correct");
+					askQuestion()
+				}
+				else {
+					alert("That's not correct =(");
+					askQuestion();
+				}
 			}
 		});
+
     }
-})
+
+	updateAnswers();
+	askQuestion();
+		
+});
 
 //Asks a random sports question to the user//
 function askQuestion () {
-    const questions = ["How many test match wickets does Shane Warne have?", "How many times have Livepool won the Champions League?", "Where is the Snooker UK Championship held?", "Which of these rugby teams are NOT part of the 6Nations?", "Who has won the men's singles at Wimbledon the most times?"];
+    const questions = ["How many test match wickets does Shane Warne have?", "How many times have Livepool won the Champions League?", "Where is the Snooker UK Championship held?","Which of these rugby teams are NOT part of the 6Nations?", "Who has won the men's singles at Wimbledon the most times?"];
     const random = Math.floor(Math.random() * questions.length);
     let questionAsked = (random, questions[random]);
     document.getElementById("sports-question").textContent = questionAsked;
 }
 
-askQuestion();
+function updateAnswers () {
+	
+}
+
